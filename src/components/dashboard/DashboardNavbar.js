68ef58f063a3c0bd050cae7ff24c676f35ab4e9d
nafeesrawji <nafees.rawji@hotmail.com>
2018-02-05 00:32:01 -0500

    Fixed bug

--- src/components/dashboard/DashboardNavbar.js
+++ src/components/dashboard/DashboardNavbar.js
@@ -5,8 +5,8 @@ import GroupNavbarItem from "./GroupNavbarItem";
 import {toggleNavbar} from "../../redux/actions/DashboardActions";
 
 const DashboardNavbar = ({groups, expanded, toggleNavbar}) =>
-<div id={"dashboard-navbar"} style={{display:`${(expanded) ? "flex" : "none"}`}} >
-    <div id={"dashboard-navbar-items"}  style={{display: (expanded) ? "flex" : "none"}}>
+<div id={`dashboard-navbar`} className={(expanded) ? "dashboard-nav-expanded" : "dashboard-nav-collapsed"} >
+    <div id={"dashboard-navbar-items"}>
         {groups.map(groupID => <GroupNavbarItem id={groupID} key={groupID}/>)}
     </div>
 </div>;
