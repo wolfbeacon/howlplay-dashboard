f050328e45a9501bcccd6976dba973aa3d504850
Frederic Pun <frederic.pun@gmail.com>
2018-04-07 15:13:09 -0400

    More styling stuff

--- src/components/dashboard/DashboardContentHeader.js
+++ src/components/dashboard/DashboardContentHeader.js
@@ -2,13 +2,12 @@ import React from 'react';
 import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
 import EditableLabel from "../EditableLabel";
-import EditIcon from "../../media/icons/edit.png";
 
 const DashboardContentHeader = ({quizName, groupName}) =>
     <div id={"dashboard-content-header"} style={{display: `${(groupName) ? 'flex': 'none'}`}}>
         <div className={"dashboard-content-labels"}>
-            <EditableLabel text={quizName} labelClassName={"edit-label quiz-name-edit-label"} editIcon={EditIcon}/>
-            <EditableLabel text={groupName} className={"group-editable"} labelClassName={"edit-label group-name-edit-label"} editIcon={EditIcon}/>
+            <EditableLabel text={quizName} className={"edit-label"} labelClassName={"edit-label quiz-name-edit-label"}/>
+            <EditableLabel text={groupName} className={"group-editable edit-label"} labelClassName={"edit-label group-name-edit-label"}/>
         </div>
         <div id="dashboard-content-controls">
             <button className={"quiz-button"} id={"start-quiz-button"}>Start</button>
