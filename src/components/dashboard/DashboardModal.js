69b4a0d02870cb0b6cca5c627ed2993b0d1c8cb5
Frederic Pun <frederic.pun@gmail.com>
2018-04-07 22:43:15 -0400

    Styled

--- src/components/dashboard/DashboardModal.js
+++ src/components/dashboard/DashboardModal.js
@@ -14,18 +14,21 @@ const DashboardModal = ({expanded, toggleModal}) =>
         <FontAwesomeIcon onClick={toggleModal} id={"dashboard-modal-close"} icon={faTimes} size="lg"/>
       </div>
       <div className={"dashboard-modal-content"}>
-        <h2>Quiz Information</h2>
-        <select id={"modal-input-hackathon"}>
-          <option value='' selected disabled>Select a Hackathon</option>
-        </select>
-        <input id={"modal-input-name"} type="text" placeholder="Name of Quiz"/>
+        <h2 className={"modal-content-headers"}>Quiz Information</h2>
+        <div className={'dashboard-modal-meta'}>
+          <input id={"modal-input-name"} type="text" placeholder="Name of Quiz"/>
+          <select id={"modal-input-hackathon"}>
+            <option value='' selected disabled>Select a Hackathon</option>
+          </select>
+        </div>
 
-        <h2>Quiz Questions</h2>
-        <ModalQuestionItem index="1"/>
-        <ModalQuestionItem index="2"/>
-        <ModalQuestionItem index="3"/>
-        <ModalQuestionItem index="4"/>
-        <button>Submit</button>
+        <h2 className={"modal-content-headers"}>Quiz Questions</h2>
+        <div className={"dashboard-modal-inputs"}>
+          <ModalQuestionItem index="1"/>
+          <ModalQuestionItem index="2"/>
+          <ModalQuestionItem index="3"/>
+        </div>
+        <button onClick={toggleModal} id={"modal-content-submit"}>Submit</button>
       </div>
     </div>
     <div id={"dashboard-modal-backdrop"} className={"modal-default"}></div>
--- src/components/dashboard/ModalQuestionItem.js
+++ src/components/dashboard/ModalQuestionItem.js
@@ -4,12 +4,20 @@ class ModalQuestionItem extends Component {
   render() {
     return (<div className={"modal-question-item"}>
       <h3>Question {this.props.index}</h3>
-      <input id={"question-1"} type="text" placeholder="Question"/>
-      <input id={"option-" + this.props.index + "-1"} type="text" placeholder="Choice 1"/>
-      <input id={"option-" + this.props.index + "-2"} type="text" placeholder="Choice 2"/>
-      <input id={"option-" + this.props.index + "-3"} type="text" placeholder="Choice 3"/>
-      <input id={"option-" + this.props.index + "-4"} type="text" placeholder="Choice 4"/>
-      <select id={"question-" + this.props.index + "-answer"}>
+      <input id={"question-1"} className={"modal-question-question"} type="text" placeholder="Question"/>
+
+      <h4 className={"modal-question-subtitle"}>Options</h4>
+      <div className="modal-question-options">
+        <input id={"option-" + this.props.index + "-1"} type="text" placeholder="Choice 1"/>
+        <input id={"option-" + this.props.index + "-2"} type="text" placeholder="Choice 2"/>
+      </div>
+      <div className="modal-question-options">
+        <input id={"option-" + this.props.index + "-3"} type="text" placeholder="Choice 3"/>
+        <input id={"option-" + this.props.index + "-4"} type="text" placeholder="Choice 4"/>
+      </div>
+
+      <h4 className={"modal-question-subtitle"}>Answer</h4>
+      <select className={"modal-question-answer"} id={"question-" + this.props.index + "-answer"}>
         <option value='' selected disabled>Select an Answer</option>
         <option value='1'>Choice 1</option>
         <option value='2'>Choice 2</option>
