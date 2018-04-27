34ccc136e7e913d3ec0d559f0fc960eac6674eee
David Wan <dave.wan@mail.utoronto.ca>
2018-04-27 09:38:55 -0400

    Added server URL constant

--- src/components/scoreboard/DisplayScore.js
+++ src/components/scoreboard/DisplayScore.js
@@ -3,6 +3,8 @@ import TopTenBoard from './TopTen.js'
 import CountdownTimer from '../timer/CountdownTimer.js'
 import StopGameButton from '../timer/StopGameButton.js'
 
+const server = "jdbc:postgresql://howlplay-db.czfcpgzgc9ja.us-west-2.rds.amazonaws.com:5432/howlplay";
+
 class DisplayScore extends React.Component {
   render() {
     return(
