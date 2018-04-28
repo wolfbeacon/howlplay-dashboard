ffb28479e79f0a2e96edb34ba5fe9b94026059b9
David Wan <dave.wan@mail.utoronto.ca>
2018-04-28 02:10:29 -0400

    added WebSocket constant to DisplayScore

--- src/components/scoreboard/DisplayScore.js
+++ src/components/scoreboard/DisplayScore.js
@@ -3,7 +3,18 @@ import TopTenBoard from './TopTen.js'
 import CountdownTimer from '../timer/CountdownTimer.js'
 import StopGameButton from '../timer/StopGameButton.js'
 
+// url is temporary, change it to the appropiate websocket link later
+const url = "ws://localhost:3000";
+const socket = new WebSocket(url);
+
 class DisplayScore extends React.Component {
+  componentWillMount() {
+    // This is just to prevent warnings with Travis. Replace/remove it whenever
+    this.socket.onopen = () => {
+      socket.send("Hello World");
+    };
+  }
+
   render() {
     return(
       <div className="display-score-screen">
