f086c691d07c047812b70531e60bc2705cae43a1
Frederic Pun <frederic.pun@gmail.com>
2018-05-10 01:44:14 -0400

    Integrated gamepage branch stuff

--- src/components/scoreboard/DisplayScore.js
+++ src/components/scoreboard/DisplayScore.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import TopTenBoard from './TopTen.js'
-import CountdownTimer from '../timer/CountdownTimer.js'
-import StopGameButton from '../timer/StopGameButton.js'
 import ScoreBoardSocketApi from '../../lib/socket.js'
 import util from '../../lib/util';
 import {bindActionCreators} from "redux"
@@ -10,10 +8,12 @@ import {getPlayers} from '../../redux/actions/ScoreboardActions'
 
 // url is temporary, change it to the appropiate websocket link later
 const url = "ws://localhost:9090";
+const answers = [0, 2, 1, 2, 1];
 const api = new ScoreBoardSocketApi(url);
 
 const server = "jdbc:postgresql://howlplay-db.czfcpgzgc9ja.us-west-2.rds.amazonaws.com:5432/howlplay";
-var users = null;
+var users = [];
+var len = 0;
 
 function getCode (buf) {
     let dataView = new Uint8Array(buf);
@@ -28,34 +28,40 @@ class DisplayScore extends React.Component {
     }
 
     api.socket.onmessage = (e) => {
-      console.log(e);
       var code = getCode(e.data);
-      if (code === 14) {
+      if (code == 14) {
         var data = new Uint8Array(e.data);
         users = JSON.parse(util.arrayBufferToString(data.slice(1)));
-        var userlist = document.querySelector('#player-list');
-        users.forEach((user) => {
-          var entry = document.createElement('div');
-          entry.innerHTML = user;
-          userlist.appendChild(entry);
-        });
+        console.log(users);
+        // var userlist = document.querySelector('#player-list');
+        // users.forEach((user) => {
+        //   var entry = document.createElement('div');
+        //   entry.innerHTML = user;
+        //   userlist.appendChild(entry);
+        // });
       }
-
     }
+
+    setInterval(() => { api.socket.sendCode(14); }, 1000);
+    setInterval(() => { api.socket.sendCode(0); }, 2000);
   }
 
   render() {
     // remove this later. Only meant for preventing Travis errors
 
     return(
-      <div className="display-score-screen">
-        <TopTenBoard serverlink={server} />
-        <CountdownTimer />
-        <div className="active-users">Active Users: 99999</div>
-        <div id="player-list">Current Players</div>
-        <StopGameButton />
+      <div id="scoreboard">
+        <div className="display-score-screen">
+          <div className="display-card">
+            <TopTenBoard users={users}/>
+          </div>
+          <div className="display-card">
+            <h2 className="time-left">Time Left <span id="timer-time">0:10</span></h2>
+            <h3 className="active-users">Active Users: <span id="users-count">{users.length}</span></h3>
+            <button id="btn-stop-game">End Game</button>
+          </div>
+        </div>
       </div>
-
     );
   }
 }
