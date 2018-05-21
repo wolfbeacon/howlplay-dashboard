dc5e846b95ae189f941b5cea3f68d422fd3b3007
nafeesrawji <nafees.rawji@hotmail.com>
2018-05-21 12:56:07 -0400

    Added validation to quiz

--- src/components/dashboard/DashboardModal.js
+++ src/components/dashboard/DashboardModal.js
@@ -1,3 +1,5 @@
+/*eslint no-useless-escape: "off"*/
+
 import React from "react";
 import {bindActionCreators} from "redux";
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
@@ -6,9 +8,45 @@ import {connect} from "react-redux";
 import ModalQuestionItem from './ModalQuestionItem';
 import {toggleModal} from "../../redux/actions/ModalActions";
 import {Form, NestedField, Text} from "react-form";
+import _ from 'lodash';
+
+const validURL = value => {
+    const pattern =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    return (!pattern.test(value) || validateNotEmpty(value)) ? "Invalid URL" : null;
+};
+
+export const validateNotEmpty = value => {
+    return (!value || value.trim() === '') ? "Field can't be empty" : null;
+};
+
+export const validateNotEmptyAsync = value =>
+    new Promise((resolve, reject) =>
+        {
+            if (validateNotEmpty(value)) {
+                reject("Field can't be empty");
+            } else {
+                resolve();
+            }
+        });
+
+const validateURLAsync = value =>
+    new Promise((resolve, reject) => {
+       if (validURL(value))  {
+           reject("Invalid URL");
+       } else {
+           resolve();
+       }
+    });
+
+const showError = (name, formAPI) => {
+    if (_.get(formAPI.touched, name) && _.get(formAPI.validationFailed, name)) {
+        return "form-error";
+    }
+    return "";
+};
 
 const DashboardModal = ({expanded, toggleModal}) =>
-    <Form onSubmit={submittedValues => {console.log(submittedValues);}}>
+    <Form onSubmit={submittedValues => {console.log(submittedValues); toggleModal();}}>
         {formApi => (
             <form id={"dashboard-modal"} onSubmit={formApi.submitForm} style={{display: `${(expanded) ? "flex" : "none"}`}}>
                 <div id={"dashboard-modal-box"} className={"modal-default"}>
@@ -19,9 +57,8 @@ const DashboardModal = ({expanded, toggleModal}) =>
                     <div className={"dashboard-modal-content"}>
                         <h2 className={"modal-content-headers"}>Quiz Information</h2>
                         <div className={'dashboard-modal-meta'}>
-                            <Text id={"modal-input-name"} field="name" placeholder="Name of Quiz"/>
-                            <Text id={"modal-input-name"} field="url" placeholder="Name of Quiz"/>
-
+                            <Text className={showError("name", formApi)} field="name" placeholder="Name of Quiz" validate={validateNotEmpty} asyncValidate={validateNotEmptyAsync}/>
+                            <Text className={showError("url", formApi)} field="url" placeholder="Game-Server URL" validate={validURL} asyncValidate={validateURLAsync}/>
                             {/*<select id={"modal-input-hackathon"}>*/}
                                 {/*<option value='' selected disabled>Select a Hackathon</option>*/}
                             {/*</select>*/}
@@ -29,9 +66,9 @@ const DashboardModal = ({expanded, toggleModal}) =>
 
                         <h2 className={"modal-content-headers"}>Quiz Questions</h2>
                         <div className={"dashboard-modal-inputs"}>
-                            {Array(3).fill().map((x,i)=> <NestedField field={["questions", i]}><ModalQuestionItem key={i} index={i + 1}/></NestedField>)}
+                            {Array(3).fill().map((x,i)=> <NestedField field={["questions", i]} key={i}><ModalQuestionItem formApi={formApi} index={i + 1}/></NestedField>)}
                         </div>
-                        <button onClick={() => {toggleModal(); formApi.resetAll()}} type="submit" id={"modal-content-submit"}>Submit</button>
+                        <button type="submit" id={"modal-content-submit"}>Submit</button>
                     </div>
                 </div>
                 <div id={"dashboard-modal-backdrop"} className={"modal-default"}/>
--- src/components/dashboard/ModalQuestionItem.js
+++ src/components/dashboard/ModalQuestionItem.js
@@ -1,5 +1,23 @@
-import React, {Component} from "react";
+import React from "react";
 import {Select, Text} from "react-form";
+import _ from 'lodash';
+import {validateNotEmptyAsync} from "./DashboardModal";
+
+const showErrorQuizName = (questionIndex, formAPI) => {
+    if (_.get(formAPI.touched, `questions[${questionIndex - 1}].question-name`)
+        && _.get(formAPI.validationFailed, `questions[${questionIndex - 1}].question-name`)) {
+        return "form-error";
+    }
+    return "";
+};
+
+const showErrorAnswers = (index, questionIndex, formAPI) => {
+    if (_.get(formAPI.touched, `questions[${questionIndex - 1}].answers[${index}]`)
+        && _.get(formAPI.validationFailed, `questions[${questionIndex - 1}].answers[${index}]`)) {
+        return "form-error";
+    }
+    return "";
+};
 
 const Answers = [
     {
@@ -20,27 +38,28 @@ const Answers = [
     },
 ];
 
-class ModalQuestionItem extends Component {
-    render() {
-        return (
-            <div className={"modal-question-item"}>
-                <h3>Question {this.props.index}</h3>
-                <Text className={"modal-question-question"} placeholder="Question Name" field={"question-name"}/>
+const ModalQuestionItem = ({index, formApi}) =>
+    <div className={"modal-question-item"}>
+        <h3>Question {index}</h3>
+        <Text className={`modal-question-question ${showErrorQuizName(index, formApi)}`} placeholder="Question Name"
+              field={"question-name"} asyncValidate={validateNotEmptyAsync}/>
 
-                <h4 className={"modal-question-subtitle"}>Options</h4>
-                <div className="modal-question-options">
-                    <Text field={["answers", 0]} placeholder="Choice 1"/>
-                    <Text field={["answers", 1]} placeholder="Choice 2"/>
-                </div>
-                <div className="modal-question-options">
-                    <Text field={["answers", 2]} placeholder="Choice 3"/>
-                    <Text field={["answers", 3]} placeholder="Choice 4"/>
-                </div>
+        <h4 className={"modal-question-subtitle"}>Options</h4>
+        <div className="modal-question-options">
+            <Text field={["answers", 0]} className={showErrorAnswers(0, index, formApi)} placeholder="Choice 1"
+                  asyncValidate={validateNotEmptyAsync}/>
+            <Text field={["answers", 1]} className={showErrorAnswers(1, index, formApi)} placeholder="Choice 2"
+                  asyncValidate={validateNotEmptyAsync}/>
+        </div>
+        <div className="modal-question-options">
+            <Text field={["answers", 2]} className={showErrorAnswers(2, index, formApi)} placeholder="Choice 3"
+                  asyncValidate={validateNotEmptyAsync}/>
+            <Text field={["answers", 3]} className={showErrorAnswers(3, index, formApi)} placeholder="Choice 4"
+                  asyncValidate={validateNotEmptyAsync}/>
+        </div>
 
-                <h4 className={"modal-question-subtitle"}>Answer</h4>
-                <Select options={Answers} field={"answer"} className={"modal-question-answer"}/>
-            </div>);
-    }
-}
+        <h4 className={"modal-question-subtitle"}>Answer</h4>
+        <Select options={Answers} field={"answer"} className={"modal-question-answer"}/>
+    </div>;
 
 export default ModalQuestionItem;
